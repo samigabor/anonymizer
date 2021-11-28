b47699e77dc9cb2be09d59cdcd287591833527bf
s-gabor <ing.gabor@yahoo.com>
2021-11-29 00:55:36 +0200

    Updates

---
+++ precache-manifest.0442af9ae3fea83a37c2873f471739cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d956c29cb8c85abcd13355610e1f733f",
+    "url": "/anonymizer/index.html"
+  },
+  {
+    "revision": "3329b9c31da36b3205c4",
+    "url": "/anonymizer/static/css/2.cdbe2572.chunk.css"
+  },
+  {
+    "revision": "0d0ab0bc6ced502c2ada",
+    "url": "/anonymizer/static/css/main.201554a6.chunk.css"
+  },
+  {
+    "revision": "3329b9c31da36b3205c4",
+    "url": "/anonymizer/static/js/2.f704d536.chunk.js"
+  },
+  {
+    "revision": "0d0ab0bc6ced502c2ada",
+    "url": "/anonymizer/static/js/main.87baad4d.chunk.js"
+  },
+  {
+    "revision": "0a7c6ec0102178bbb7e4",
+    "url": "/anonymizer/static/js/runtime-main.bc8d44b7.js"
+  }
+]);
